b88cfafd4826c1f04c9d5552cad56ad2452b854d
mattkrnr <44987169+mattkrnr@users.noreply.github.com>
2018-12-02 20:56:30 -0500

    Add files via upload

---
+++ codejs.js
@@ -0,0 +1,73 @@
+function doctypePopup() {
+	var elem = document.getElementById("doctypePopup");
+	elem.style.display = "block";
+}
+
+function doctypeExit() {
+	var elem = document.getElementById("doctypePopup");
+	elem.style.display = "none";
+}
+function aPopup() {
+	var elem = document.getElementById("aPopup");
+	elem.style.display = "block";
+}
+function aExit() {
+	var elem = document.getElementById("aPopup");
+	elem.style.display = "none";
+}
+function bodyPopup() {
+	var elem = document.getElementById("bodyPopup");
+	elem.style.display = "block";
+}
+function bodyExit() {
+	var elem = document.getElementById("bodyPopup");
+	elem.style.display = "none";
+}
+function divPopup() {
+	var elem = document.getElementById("divPopup");
+	elem.style.display = "block";
+}
+function divExit() {
+	var elem = document.getElementById("divPopup");
+	elem.style.display = "none";
+}
+function headerPopup() {
+	var elem = document.getElementById("headerPopup");
+	elem.style.display = "block";
+}
+function headerExit() {
+	var elem = document.getElementById("headerPopup");
+	elem.style.display = "none";
+}
+function imgPopup() {
+	var elem = document.getElementById("imgPopup");
+	elem.style.display = "block";
+}
+function imgExit() {
+	var elem = document.getElementById("imgPopup");
+	elem.style.display = "none";
+}function mapPopup() {
+	var elem = document.getElementById("mapPopup");
+	elem.style.display = "block";
+}
+function mapExit() {
+	var elem = document.getElementById("mapPopup");
+	elem.style.display = "none";
+}
+function linkPopup() {
+	var elem = document.getElementById("linkPopup");
+	elem.style.display = "block";
+}
+function linkExit() {
+	var elem = document.getElementById("linkPopup");
+	elem.style.display = "none";
+}
+ function tablePopup() {
+	var elem = document.getElementById("tablePopup");
+	elem.style.display = "block";
+}
+function tableExit() {
+	var elem = document.getElementById("tablePopup");
+	elem.style.display = "none";
+}
+
