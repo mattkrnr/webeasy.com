41650cc912b75bb037adee8bfdd5d6f5707c5973
Hamzah Nizami <43225896+hniz@users.noreply.github.com>
2018-12-03 16:22:52 -0500

    Add files via upload
    - adds a page called about the creators

---
+++ webeasy.com/aboutthecreators.js
@@ -0,0 +1,12 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function buttonClicked(){ 
+	modal.style.display = "block";
+}
